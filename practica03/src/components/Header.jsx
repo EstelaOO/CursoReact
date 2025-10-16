03a454c521fe2dce08b172ea1ca130af8d72e257
Estela <teliss88@gmail.com>
2025-10-16 12:07:01 -0300

    nueva actualización

---
+++ practica03/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+
+const Header = () => {
+    return(
+        <header className="bg-prymary text-white py-4 shadow-sm">
+            <Container>
+                <h1 className="">Practica 03</h1>
+                <p className="">Componentes React Friendly</p>
+            </Container>
+
+        </header>
+    )
+}
